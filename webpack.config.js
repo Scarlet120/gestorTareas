fb6525afa8fbc4bfc865d5c17846869879e0c2c6
Scarlet Estrada <scarletmaldonado12@gmail.com>
2024-07-23 18:54:09 -0600

    Estructura del proyecto,config de webpack e inicio de la estructura HTML

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+     entry: './src/index.js', //Punto de entrada 
+     outpun: {
+        filename: 'bundle.js', //Nombre del archivo de salida
+        path: path.resolve(__dirname, 'dist'), //Carpeta de salida
+     }
+     module: {
+        rules: [
+            {
+                test: /\.css$/, //Regex para identificar archivos CSS
+                use: ['style-loadar', 'css-loader'], //Loaders para procesar archivos 
+            },
+            {
+                test: /\.js$/, //Regex para identificar archivos JS
+                exclude: /node_modules/, //Excluir la carpeta node_modules
+                use: {
+                    loader: 'babel-loader', //Loader para convertir JS moderno al JV
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+     },  
+     devtool: 'source-map', //Genera source maps para facilitar la depuración 
+     devServer: {
+        contentBase: path.resolve(__dirname, 'dist'), //Carpeta del que correra el servidor
+        compress: true, //Habilitar compresión gzip 
+        port: 9000, //Puerto del servidor de desarrollo
+     },
+    };
